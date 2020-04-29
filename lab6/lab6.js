3efdcaa10cc74be8ce63dff7b0b199ec0bf4f428
mikollin <aichaodieqin@126.com>
2020-04-29 19:57:57 +0800

    handin lab6

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -11,9 +11,29 @@
 */
 
 function testTime(){
-
+    let value=1;
+    let i=0;
+    console.log(value);
+    let interval=setInterval(function(){
+        value=value*2;
+        i=i+1;
+        console.log(value);
+        if(new Date().getSeconds()===0||i===10) {
+            console.log('已运行10次！停止！')
+            clearInterval(interval);
+            clearInterval(stop);
+        }
+    },5000);//页面载入后，每经过指定毫秒值后执行指定表达式，是间隔多次执行的
+    let stop =setInterval(function () {
+        if (new Date().getSeconds()===0){
+            console.log('一分钟到了！停止!')
+            clearInterval(stop);
+            clearInterval(interval);
+        }
+    },1);
+    
 }
-// testTime();
+//testTime();
 
 /*
 2.
@@ -24,6 +44,26 @@ function testTime(){
     ④telephone与mail均是字符串。
 */
 function testMail(telephone,mail) {
+    let reTel=/^1(3|4|5|7|8)\d{9}$/;
+    let reMail=/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    //邮箱的基本格式为“名称@域名”，需要使用“^”匹配邮箱的开始部分，用“$”匹配邮箱结束部分以保证邮箱前后不能有其他字符
+    let telr=reTel.test(telephone);
+    let mailr=reMail.test(mail);
+    if(telr&&mailr){
+        console.log("The telephone and the mail are both right!");
+    }
+    else if(telr&&(!mailr)){
+        console.log("The telephone is right and the mail is wrong!");
+    }
+    else if((!telr)&&mailr){
+        console.log("The telephone is wrong and the mail is right!");
+    }
+    else{
+        console.log("The telephone and the mail are both wrong!");
+    }
+
+
+
 
 }
 
@@ -37,10 +77,63 @@ function testMail(telephone,mail) {
     ⑤str为字符串。
 */
 function testRedundancy(str) {
+    function compare(a,b){//首字母比较
+        if((a.toLocaleUpperCase())[0]===(b.toLocaleUpperCase())[0])
+            return a-b;//ASCII 从小到大
+        else
+            return a.localeCompare(b)
+    }
+    let reg=/\b([a-zA-z]+) \1\b/ig;
+    //\b是匹配一个单词的边界 \1是引用第一个括号的内容
+
+    /* 原先的错误做法orzzz
+    let set=new Set();
+     set.add(str.match(reg));
+
+    let value;
+    let result="";
+    for(let x of set){
+        if(x.length>10){
+            x.sort(compare);
+            x=x.slice(0,10)
+        }
+        x.sort(compare);
+        value=x.toString().split(",");
+    }
+
+    for(let x of value){
+        result+="'"+x+"',";
+        //console.log(result);
+    }
+    result=result.substring(0,result.length-1);
+    console.log("Set { "+result+" }");
+   //console.log(set);
+*/
+
+    let array=str.match(reg);
+    array.sort(compare);
+    let result=new Array();
+    let set=new Set(array);
+    if(set.size>10) {
+
+        for(i=10;i<array.length;i++) {
+            result = array.slice(0, i);
+            set = new Set(result);
+            if (set.size === 10)
+                break;
+
+        }
+        console.log(set);
+    }
+    else{
+
+        console.log(set);
+    }
+
 
 }
-
-
+//testRedundancy("Is is the iS is  up up cost of of GoIng going let Let new New we We is to To go Go and And FF ff");
+//testRedundancy("Is is the iS is cost of of gasoline going up up");
 /*
 4.
 背景：
@@ -57,7 +150,29 @@ function testRedundancy(str) {
 */
 function testKeyBoard(wantInput, actualInput) {
 
+    wantInput=wantInput.toLocaleUpperCase();
+    actualInput=actualInput.toLocaleUpperCase();
+    let len=wantInput.length;
+
+    let set=new Set();
+    for(let i=0,j=0;i<len&&j<len;){
+        // let pattern='/'+actualInput[i]+'/'+'i';
+        // let reg=pattern;
+        if(wantInput[i]===actualInput[j]) {
+            i++;
+            j++;
+        }
+        else{
+                set.add(wantInput[i].toLocaleUpperCase());
+                //result+="'"+wantInput[i].toLocaleUpperCase()+"' ,";
+                i++;
+        }
+
+    }
+    console.log(set);
+
 }
+// testKeyBoard("7_This_is_a_test", "_hs_s_a_es");
 
 /*
 5.
@@ -72,8 +187,21 @@ function testKeyBoard(wantInput, actualInput) {
     ⑤str为字符串。
 */
 function testSpecialReverse(str) {
-}
+    let array=new Array();
+    let result=new Array();
+    array=str.split(" ");
+    //console.log(array);
+    for(let i=array.length-1;i>=0;i--){
+        if(array[i]==='')
+            continue;
+        result.push(array[i]);
 
+    }
+    result=result.join(" ");
+    console.log("\""+result.toString()+"\"");
+}
+//testSpecialReverse("  hello  world!  ");
+//testSpecialReverse("the sky is blue");
 /*
 6.
 背景：
@@ -90,8 +218,25 @@ function testSpecialReverse(str) {
 */
 
 function twoSum(nums, target) {
-}
+    let result=[];
+    let map=new Map();
+    for(let i=0;i<nums.length;i++){
+        result[target-nums[i]] = i;
+        if(result[nums[i]] !==undefined){
+           map.set(result[nums[i]],i);
+        }
 
+    }
+
+    result=Array.from(map);
+    result.sort();
+    result=result.join(']\n[');
+
+    console.log('['+result+']');
+
+
+}
+// twoSum([1,2,3,4], 5);
 
 /*
 7.
@@ -99,13 +244,35 @@ function twoSum(nums, target) {
     打印最长的包含不同字符串的子字符串长度。
 要求：
     ①使用Map。
-    ②例如：输入"abbbbb",输出1，输入"bbbbb",输出2；
+    ②例如：输入"abbbbb",输出2，输入"bbbbb",输出1；
     ③只能显式使用一次循环。
     ④使用console.log打印即可。
     ⑤str为字符串。
 */
 function lengthOfLongestSubstring(str) {
+
+        let result='';//当前最长字串
+        let lens=0;
+        let char;
+        let index;
+        for(let i=0;i<str.length;i++){
+            char=str.charAt(i);
+            index=result.indexOf(char);
+            if(index===-1){
+                result+=char;
+                lens=lens<result.length?result.length:lens;
+            }
+            else{
+                result=result.substr(index+1)+char;
+            }
+        }
+
+        console.log(lens);
+
 }
+//lengthOfLongestSubstring("abbcdcbbb");
+//lengthOfLongestSubstring("bbbbb");
+
 
 /*
 8.
@@ -119,3 +286,68 @@ function lengthOfLongestSubstring(str) {
 function Country() {
     this.name = "国家";
 }
+
+function DevelopingCountry(name){
+    Country.call(this,name);//借助构造函数
+}
+DevelopingCountry.prototype.sayHi=function(){
+   return "Hi,i am a developing country.";
+     //console.log()默认接受函数时会打印返回值，如果没有设定，会返回 undefined。
+}
+ let developingCountry=new DevelopingCountry("developing country");
+
+function PoorCountry(){
+
+}
+PoorCountry.prototype=new Country();  //原型链
+PoorCountry.prototype.saySad=function () {
+    //console.log("I am a sad poor country.");
+    return "I am a sad poor country.";
+}
+let poorCountry=new PoorCountry();
+
+Country.prototype.sayHappy=function(){
+    //console.log("I am a Happy developed country.");
+    return "I am a Happy developed country.";
+}
+  let developedCountry=Object.create(Country.prototype);//,{
+ //     sayHappy:{
+ //         value:function(){  //Object.create
+ //             return "I am a Happy developed country."
+ //         }
+ //     }
+ // });
+
+
+// console.log(developingCountry.sayHi());
+// console.log(poorCountry.saySad());
+// console.log(developedCountry.sayHappy());
+
+
+//调用函数tet
+function test(){
+    // testTime();//最后调用
+    testMail("13812238483","ai324@126.com");
+    testMail("145defdqf","edj.dejn@rdjcn.enjd.com");
+    testMail("13817772345","edj.dejn@rdjcn.enjd.com");
+    testMail("145defdqf","edj@fudan.edu.cn");
+    testRedundancy("Is is the iS is  up up cost of of iS is GoIng going let Let new New we We is to To go Go and And FF ff");
+    //testRedundancy("Is is the iS is cost of of gasoline going up up");
+    testKeyBoard("7_This_is_a_test", "_hs_s_a_es");
+    testSpecialReverse("  hello  world!  ");
+    //testSpecialReverse("the sky is blue");
+    twoSum([1,2,3,4], 5);
+    //lengthOfLongestSubstring("abbcdcbbb");
+    lengthOfLongestSubstring("abbbbb");
+
+
+
+    console.log(developingCountry.sayHi());
+    console.log(poorCountry.saySad());
+    console.log(developedCountry.sayHappy());
+
+    testTime();
+}
+test();
+
+
---
+++ lab6/lab6设计文档.md
@@ -0,0 +1,212 @@
+## lab6设计文档
+
+——18300290055 赵一玲
+
+##### Tips
+
+最后我编写并调用函数test来调用每一题的函数看结果，这里因为第一题的时间比较长，放到了最后再调用。
+
+#### 一些截图
+
+本地命令行截图：
+
+<img src="/Users/yilingzhao/Desktop/web/SOFT130002_lab/lab6/img/lab6截图1.png" alt="lab6截图1" style="zoom:100%;" />
+
+![lab6截图2](/Users/yilingzhao/Desktop/web/SOFT130002_lab/lab6/img/lab6截图2.png)
+
+第一题整体的情况如下（为了方便这边直接列出两种情况）：
+
+![lab6-1](/Users/yilingzhao/Desktop/web/SOFT130002_lab/lab6/img/lab6-1.png)
+
+#### 正则表达式的解释
+
+请看每一题中逐个讲解。
+
+#### Map、Set、Array之间的区别和使用
+
+区别：
+
+Map是映射，其中存放键值对，同一个键多次赋值会覆盖，描述映射，没有sort方法，没有slice方法，有set可添加。
+
+Set中每个成员唯一，所以可以用来过滤重复元素，可以接受数组对象作为参数，通过size属性得知个数，没有sort方法，没有slice方法，有add可添加。
+
+Array是数组，可重复，可与map转换，如Array.from(map)，有length属性得知个数，有其特有的方法：sort，join，slice，push可添加。
+
+具体使用及其使用原因请看每一题中逐个讲解。
+
+#### 继承不同方式的理解
+
+构造函数的继承：在子类构造函数中用call调用父类的构造函数，得到其属性值，再向其子类原型增加方法之后再实例化。但是父类原型中的方法（和属性）对子类不可见，父类实例方法无法被复用。
+
+原型链的继承：将子类的原型设为父类的实例，之后可以增加方法/覆盖父类方法。可以在构造函数中设置子类特有的属性。但其无法在不影响所有子类实例的情况下向父类构造函数传参。
+
+Object.create的继承：将第一个参数设为我要创建的对象的原型，本质上对已有对象做了浅复制，是原型式继承。
+
+具体的继承操作看第八题中的详解。
+
+#### 第一题
+
+在testTime（）中建立内部函数，闭包，一个是interval，其中为计算value翻倍的代码，同时每次运行计数，到10次停止，每隔5s运行一次。另一个是stop,每隔1ms查看一次保证一旦到了整分立刻停止,不用管是否是5为倍数的整秒调用。
+
+同时由于这次两个setInterval都是在testTime中的，两个都要清除彼此。
+
+#### 第二题
+
+关于电话号码的正则表达式的理解：
+
+【参考的规则原文链接：http://caibaojian.com/phone-regexp.html】
+
+中国电信手机号码开头数字
+2G/3G号段（CDMA2000网络）133、153、180、181、189
+4G号段 177
+
+中国联通手机号码开头数字
+2G号段（GSM网络）130、131、132、155、156
+3G上网卡145
+3G号段（WCDMA网络）185、186
+4G号段 176、185[1]
+
+中国移动手机号码开头数字
+2G号段（GSM网络）有134x（0-8）、135、136、137、138、139、150、151、152、158、159、182、183、184。
+3G号段（TD-SCDMA网络）有157、187、188
+3G上网卡 147
+4G号段 178
+
+从以上我们可以看到第一位是【1】开头，第二位则则有【3,4,5,7,8】，第三位则是【0-9】，第三位之后则是数字【0-9】。从而我们可以得出一个符合当前的手机号码验证正则表达式。
+
+于是我才用的正则表达式如下：
+
+```js
+let reTel=/^1(3|4|5|7|8)\d{9}$/;
+```
+
+即从开头，第一位1，之后是3/4/5/7/8，之后是\d重复九次，即ascii码数字重复九次。
+
+关于邮箱正则表达式的理解：
+
+邮箱的基本格式为“名称@域名”，需要使用“^”匹配邮箱的开始部分，用“$”匹配邮箱结束部分以保证邮箱前后不能有其他字符。
+
+```js
+let reMail=/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+```
+
+邮件名称表达式：`[a-zA-Z0-9_-]+` ,允许0-9数字大小写英文字母-和，之后域名部分可为“`**` `.**` `.**` `.**`”，`**` 部分可以表示`[a-zA-Z0-9_-]+`,`.**`部分即`\.[a-zA-Z0-9-]+`，而多个`.**`可以表示为`\.[a-zA-Z0-9_-]+)+`,综上，域名部分表示为：`[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+`
+
+即邮箱表达式的pattern：
+
+`^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$`
+
+之后用正则表达式的test方法来判断是否符合要求并打印。
+
+#### 第三题
+
+通过正则表达式找到相邻的重复单词放入一个set
+
+```js
+let reg=/\b([a-zA-z]+) \1\b/ig;
+```
+
+\b是匹配一个单词的边界, \1是引用第一个括号的内容,即相邻重复的英文单词，中间还应该有空格隔开。最后flag设置全局匹配和匹配过程忽略大小写。
+
+用字符串的match方法把相邻的重复英文单词的结果赋给一个array数组，建立compare函数使得比较单词为首字母排序，即AaBbCc……，如下：
+
+```js
+function compare(a,b){//首字母比较
+    if((a.toLocaleUpperCase())[0]===(b.toLocaleUpperCase())[0])
+        return a-b;//ASCII 从小到大
+    else
+        return a.localeCompare(b)
+}
+```
+
+a.localeCompare(b)是按照aabBcC……的顺序，所以加一步判断如果两个字母是相同的（不分大小写），则返回ascii码序，即大写在前小写在后。
+
+把数组重新取排序后前十个，排序完之后通过 `let set=new Set(array);`把数组转为集合，把重复的过滤，如果过滤后集合中元素仍超过十个，则经过一个循环取result为转换后的数组（用`result = array.slice(0, i);`）转换为set一直取到set的size为10为止（因为有可能array中比如有14个结果，但是有两个是重复的，且在前十个中，那么需要保证取到后五个元素加到set中保证其size为10，正确）。最后打印。
+
+#### 第四题
+
+先将wantInput和actualInput，均转换成大写然后每个字符串设置一个计数器，wantInput设置为i，actualInput设置为j，从0开始逐字比较两者，如果相同，双方均++，如果不同，我们知道键盘坏了一定是wantInput的字符多，所以此时先加到set中（由于键盘字母是大写的），wantInput的index：i++，继续比对。最后打印set。
+
+#### 第五题
+
+首先将str字符串按照空格分割到array数组中，然后倒序遍历改数组，如果不为空格则加到result中（用push方法），最后result数组用空格把数组元素链接，并且加上“”。
+
+#### 第六题
+
+先创建一个空数组result，遍历参数数组（一个for循环），进行这一步，将target-nums[i]下标的元素设为当前i：
+
+```js
+result[target-nums[i]] = i;
+```
+
+之后判断result[nums[i]] 是否为undefined，如果不是，即说明之前出现了i0，与当前i相加为target，将改对映射放入map中,参数数组遍历完成后，用Array.from(map)转换为数组，对其进行排序。同时为了得到样例输出如下设置：
+
+```js
+result=result.join(']\n[');
+
+console.log('['+result+']');
+```
+
+#### 第七题
+
+首先用一个空字符串result作为当前最长字符串，lens初始值为0。遍历参数字符串，逐字符来看，如果当前字符不在result中，加到result末尾，设置lens：`lens=lens<result.length?result.length:lens;`
+
+如果在result中，则说明有重复，取重复的后一个字符开始重新取包含不同字符的子字符串长度，如果长度小于之前的lens，则不改变，如果更长，则说明找到更长的，改变。
+
+最后打印lens，即最长的不同字符子字符串长度。
+
+#### 第八题
+
+构造函数的继承：在子类构造函数DevelopingCountry中用call调用父类的构造函数，得到其属性值，再向其子类原型增加方法
+
+```js
+DevelopingCountry.prototype.sayHi=function(){
+   return "Hi,i am a developing country.";
+     //console.log()默认接受函数时会打印返回值，如果没有设定，会返回 undefined。
+}
+```
+
+之后再实例化。这里一开始发现总是会打出一个undefined，之后发现是console.log()默认接受函数时会打印返回值，如果没有设定，会返回 undefined。所以直接返回字符串即可。
+
+原型链的继承：将子类PoorCountry的原型设为父类的实例，之后可以增加方法
+
+```js
+PoorCountry.prototype.saySad=function () {
+    //console.log("I am a sad poor country.");
+    return "I am a sad poor country.";
+}
+```
+
+之后再实例化
+
+Object.create的继承：为了让我的developedCountry对象拥有sayHappy方法先将原型中设置方法
+
+```js
+Country.prototype.sayHappy=function(){
+    //console.log("I am a Happy developed country.");
+    return "I am a Happy developed country.";
+}
+```
+
+对于将第一个参数设为我要创建的对象developedCountry的原型即Country.prototype。
+
+第二种操作可以如下：
+
+```js
+let developedCountry=Object.create(Country.prototype,{
+     sayHappy:{
+         value:function(){  //Object.create
+             return "I am a Happy developed country."
+        }
+     }
+ });
+```
+
+将方法看成一种属性，value为function。
+
+调用同样为 console.log(developedCountry.sayHappy());
+
+
+
+
+
