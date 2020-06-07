bc2cda31f733e5f1240a4e078163fc6ae3cbdf05
mikollin <aichaodieqin@126.com>
2020-06-07 21:56:48 +0800

    handin lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'root');
+define('DBPASS', 'root');
+define('DBCONNSTRING','mysql:host=localhost;dbname=Login');
+?>
--- lab11/lab11-exercise1.php
+++ lab11/lab11-exercise1.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   <title>Exercise 13-1 | Using Cookies</title>
+   <title>Exercise 11-1 | Using Cookies</title>
 </head>
 
 <body>
@@ -14,6 +14,7 @@
 
 
 <?php
+
 function getLoginForm(){
    return "
 <form action='' method='post' role='form'>
@@ -30,22 +31,68 @@ function getLoginForm(){
 </form>";
 }
 ?>
+
+<?php
+function validLogin(){
+    $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+    //very simple (and insecure) check of valid credentials.
+    $sql = "SELECT * FROM Credentials WHERE Username=:user and Password=:pass";
+
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':user',$_POST['username']);
+    $statement->bindValue(':pass',$_POST['pword']);
+    $statement->execute();
+    if($statement->rowCount()>0){
+        return true;
+    }
+    return false;
+}
+?>
+
  <div class="container theme-showcase" role="main">  
       <div class="jumbotron">
         <h1>
 <?php
    require_once("config.php");
-   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     echo "Login attempted";
-   }
-   else{
-     echo "No Post detected";
-   } 
+//   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     echo "Login attempted";
+//   }
+//   else{
+//     echo "No Post detected";
+//   }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (validLogin()) {
+        // add 1 day to the current time for expiry time
+        $expiryTime = time() + 60 * 60 * 24;
+        $_SESSION['Username']=$_POST['username'];
+
+    } else {
+        echo "login unsuccessful";
+
+    }
+
+    if (isset($_SESSION['Username'])) {
+        echo "Welcome " . $_SESSION['Username'];
+        echo "<a href='logout.php'>Logout</a>";
+    }
+}
+else{
+    echo "No Post detected";
+}
+
 ?>
 
 </h1>
       </div>
-<?php echo getLoginForm(); ?>
+<?php
+if (!isset($_SESSION['Username'])){
+    echo getLoginForm();
+}
+else{
+    echo "This is some content";
+}
+?>
  </div>
 </body>
 </html>
